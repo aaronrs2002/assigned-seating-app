dc0b035345a9c43d39750d14dfc518dd5d2c50bb
Aaron Smith <aaron@web-presence.biz>
2026-06-04 12:19:12 -0700

    building event task details2

--- js/functionality.js
+++ js/functionality.js
@@ -126,7 +126,7 @@ function selectProfile() {
             for (let i = 0; i < guestData[whichProfile].events.length; i++) {
                 if (usedAssigned.indexOf(guestData[whichProfile].events[i].task) === -1) {
                     seatAssignmentHTML = seatAssignmentHTML + `<li class="list-group-item"><label>${guestData[whichProfile].events[i].task} seat</label><input type="text" class="form-control" name="${guestData[whichProfile].events[i].task}-seat" value="${guestData[whichProfile].events[i].seat}" placeholder="Assigned Seat for ${guestData[whichProfile].events[i].task}"/>
-                    <div><label>${guestData[whichProfile].events[i].task} Detials</label><textarea class="form-control" name="${guestData[whichProfile].events[i].task}-details" value="${guestData[whichProfile].events[i].details}"></textarea> </div>
+                    <div><label>${guestData[whichProfile].events[i].task} Detials</label><textarea class="form-control" name="${guestData[whichProfile].events[i].task}-details" >${guestData[whichProfile].events[i].details}</textarea> </div>
                     </li>`
 
                     usedAssigned.push(guestData[whichProfile].events[i].task);
@@ -544,7 +544,7 @@ function updateProfileTask() {
                 user: activeUser, task: e.value, seat: seatVal, details: detailsVal
             });
             seatAssignmentHTML = seatAssignmentHTML + `<li class="list-group-item"><label>${e.value} seat</label><input type="text" class="form-control" value="${seatVal}" name="${e.value}-seat" placeholder="Assigned Seat ${e.value}"/>
-            <div><label>${e.value} Details</label><textarea name="${e.value}-details" value="${detailsVal}" class="form-control"></textarea> </div>
+            <div><label>${e.value} Details</label><textarea name="${e.value}-details" class="form-control">${detailsVal}</textarea> </div>
             </li>`;
         }
 
